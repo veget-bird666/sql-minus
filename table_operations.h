ae14f88bb011232766f4663ad089b9b2fc39013d
veget-bird666 <23301164@bjtu.edu.cn>
2025-03-23 16:44:41 +0800

    Update table_operations.h

--- table_operations.h
+++ table_operations.h
@@ -1,4 +1,22 @@
 #ifndef TABLE_OPERATIONS_H
 #define TABLE_OPERATIONS_H
+#include "operation.h"
+#include "structures.h"
+#include <vector>
+using namespace std;
+
+class TableOperation : public Operation {
+
+};
+
+
+class CreateTableOperation : public TableOperation {
+public:
+    TableBlock table_block;                     // 表定义
+    vector<FieldBlock> field_blocks;            // 字段
+    vector<IntegrityConstraint> constraints;  // 新增完整性约束
+    vector<IndexBlock> indexes;               // 新增索引
+};
+
 
 #endif // TABLE_OPERATIONS_H
