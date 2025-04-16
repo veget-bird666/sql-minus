e420c2f77dcff14226375b134c11b0211c470e54
veget-bird666 <23301164@bjtu.edu.cn>
2025-04-16 19:06:34 +0800

    完成对表字段的添加、删除、修改

--- table_operations.h
+++ table_operations.h
@@ -10,7 +10,7 @@ class TableOperation : public Operation {
 
 };
 
-
+// 创建表的操作类
 class CreateTableOperation : public TableOperation {
 private:
     void extractIntegrityConstraints(const FieldBlock& field);
@@ -23,6 +23,78 @@ public:
     QString dbName;                             // 所在数据库名称
 
     CreateTableOperation(QString, QList<FieldBlock>);
+    void execute() override;
+};
+
+// 显示所有表子类
+class ShowTablesOperation : public TableOperation {
+public:
+    explicit ShowTablesOperation() : TableOperation() {}
+    void execute() override;
+};
+
+
+// 删除表的操作类
+class DropTableOperation : public TableOperation {
+public:
+    QString dbName;
+    QString tableName;
+
+    explicit DropTableOperation(const QString& dbName, const QString& tableName)
+        : dbName(dbName), tableName(tableName) {}
+
+    void execute() override ;
+};
+
+
+// 添加字段操作类
+class AddColumnOperation : public TableOperation {
+public:
+    QString dbName;
+    QString tableName;
+    FieldBlock newField;
+
+    explicit AddColumnOperation(const QString& db, const QString& table, const FieldBlock& field)
+        : dbName(db), tableName(table), newField(field) {}
+
+    void execute() override;
+};
+
+// 删除字段操作类
+class DropColumnOperation : public TableOperation {
+public:
+    QString dbName;
+    QString tableName;
+    QString columnName;
+
+    explicit DropColumnOperation(const QString& db, const QString& table, const QString& column)
+        : dbName(db), tableName(table), columnName(column) {}
+
+    void execute() override;
+};
+
+// 修改字段操作类
+class ModifyColumnOperation : public TableOperation {
+public:
+    QString dbName;
+    QString tableName;
+    QString oldColumnName;
+    FieldBlock newField;
+
+    explicit ModifyColumnOperation(const QString& db, const QString& table,
+                                   const QString& oldName, const FieldBlock& field)
+        : dbName(db), tableName(table), oldColumnName(oldName), newField(field) {}
+
+    void execute() override;
+};
+
+// 描述表的操作类
+class DescribeTableOperation : public TableOperation {
+public:
+    QString dbName;
+    QString tableName;
+    explicit DescribeTableOperation(const QString& dbName, const QString& tableName)
+        : dbName(dbName), tableName(tableName) {}
     void execute() override;
 };
 
