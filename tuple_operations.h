aef413dca16c26cffdf643c58be650de960624c4
veget-bird666 <23301164@bjtu.edu.cn>
2025-05-06 18:22:38 +0800

    Update tuple_operations.h

--- tuple_operations.h
+++ tuple_operations.h
@@ -1,4 +1,20 @@
 #ifndef TUPLE_OPERATIONS_H
 #define TUPLE_OPERATIONS_H
+#include "operation.h"
+#include "structures.h"
+class TupleOperation : public Operation{
+
+};
+
+// 插入数据操作
+class InsertOperation : public TupleOperation {
+public:
+    QString dbName;          // 目标数据库
+    QString tableName;       // 目标表
+    std::vector<FieldValue> values; // 按字段顺序存储的值
+    void execute() override;
+};
 
 #endif // TUPLE_OPERATIONS_H
+
+
