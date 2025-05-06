0f08aee692bc527fc5afc528d0cda79c46c271ef
veget-bird666 <23301164@bjtu.edu.cn>
2025-05-06 18:22:42 +0800

    Update tuple_manager.h

--- tuple_manager.h
+++ tuple_manager.h
@@ -1,4 +1,17 @@
 #ifndef TUPLE_MANAGER_H
 #define TUPLE_MANAGER_H
 
+#include "manager.h"
+#include "structures.h"
+#include <vector>
+#include <stdexcept>
+#include "tuple_operations.h"
+
+
+class TupleManager : public Manager {
+public:
+    static void insert(const InsertOperation* op);
+
+};
+
 #endif // TUPLE_MANAGER_H
