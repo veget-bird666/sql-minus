382521d89745fe226d3628f1f4b162bba42d48f0
veget-bird666 <23301164@bjtu.edu.cn>
2025-05-07 18:16:32 +0800

    Update sqlparser.h
    实现简单插入、删除、select *

--- sqlparser.h
+++ sqlparser.h
@@ -14,6 +14,15 @@ public:
     static Operation* parse(const QString& sql);
     static QList<FieldBlock>  extractFields(const QString& tableDefinition);
 
+    // 解析WHERE条件
+    static std::vector<Condition> parseWhereClause(const QString& whereClause, const std::vector<FieldBlock>& fields);
+
+private:
+    // 解析单个条件表达式
+    static Condition parseSingleCondition(const QString& conditionStr, const std::vector<FieldBlock>& fields);
+
+    // 将字符串转换为FieldValue
+    static FieldValue stringToFieldValue(const QString& str, DataType type);
 
 };
 
