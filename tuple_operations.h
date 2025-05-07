2adae222589e02f90056f1937eac21edfaecaff9
veget-bird666 <23301164@bjtu.edu.cn>
2025-05-07 18:15:54 +0800

    Update tuple_operations.h
    实现简单插入、删除、select *

--- tuple_operations.h
+++ tuple_operations.h
@@ -20,13 +20,27 @@ public:
 };
 
 
-class SelectAllOperation : public Operation {
+class SelectAllOperation : public TupleOperation {
 public:
     QString dbName;
     QString tableName;
 
     SelectAllOperation(const QString& dbName, const QString& tableName)
         : dbName(dbName), tableName(tableName) {}
+
+    void execute() override;
+};
+
+
+
+class DeleteOperation : public TupleOperation {
+public:
+    QString dbName;
+    QString tableName;
+    std::vector<Condition> conditions;
+
+    DeleteOperation(const QString& db, const QString& table, const std::vector<Condition>& conds)
+        : dbName(db), tableName(table), conditions(conds) {}
 
     void execute() override;
 };
