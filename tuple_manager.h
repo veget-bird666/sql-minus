f2084a9f184206c8b56f6c6def45bf6ef0aeed69
shfhdfcserfw <wheazebaly@163.com>
2025-05-08 17:51:15 +0800

    Update tuple_manager.h

--- tuple_manager.h
+++ tuple_manager.h
@@ -16,7 +16,28 @@ public:
     static void selectAll(const SelectAllOperation* operation);
     // 删除记录
     static void deleteRows(const DeleteOperation* op);
-
+    // 查询部分记录
+    static void selectColumns(const SelectColumnsOperation* op);
+private:
+    int compareFieldValues(const FieldValue& v1, const FieldValue& v2) const;
+    FieldValue calculateFunction(
+        const FieldBlock& funcField,
+        const std::vector<FieldBlock>& fields,
+        const std::vector<DataRow>& rows
+        );
+    QString formatValue(const FieldValue& val, DataType type);
+    void handleAggregateSelect(
+        const SelectColumnsOperation* op,
+        const std::vector<FieldBlock>& fields,
+        const std::vector<DataRow>& rows,
+        const std::vector<int>& columnIndices);
+    QString formatFunctionValue(const FieldValue& val);
+    QString formatFieldValue(const FieldValue& val);
+    void handleRegularSelect(
+        const SelectColumnsOperation* op,
+        const std::vector<FieldBlock>& fields,
+        const std::vector<DataRow>& rows,
+        const std::vector<int>& columnIndices);
 };
 
 #endif // TUPLE_MANAGER_H
