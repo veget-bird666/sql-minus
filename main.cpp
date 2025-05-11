eef0acffa643c9cdbce13db0b86382e97db8a01c
veget-bird666 <23301164@bjtu.edu.cn>
2025-05-11 15:24:33 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,9 +22,11 @@ int main(int argc, char *argv[])
  * create database db01;
  * drop database db02;
  * use database db03;
+ *
  * show tables;
  * create table t04(...);
  * drop table t04;
+ *
  * alter table t05 add column c01 int not null;
  * alter table t06 drop column c02;
  * alter table t07 modify column c03 int;
@@ -33,4 +35,8 @@ int main(int argc, char *argv[])
  * DELETE FROM t06 WHERE age > 16;
  * DELETE FROM t06 WHERE age = 18;
  * SELECT * FROM t06;
+ * SELECT (sno , sname ) FROM t01 WHERE age > 18;
+ * UPDATE employees SET salary = 5000, department = 'IT' WHERE id = 123;
+ * UPDATE products SET price = price * 1.1 WHERE category = 'Electronics';
+
  */
