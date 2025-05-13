372e0088a2b41179eeccbf5ca1891f558c63ba2b
veget-bird666 <23301164@bjtu.edu.cn>
2025-05-13 09:46:54 +0800

    Update operation.h
    实现日志、sql脚本解析

--- operation.h
+++ operation.h
@@ -1,10 +1,16 @@
 #ifndef OPERATION_H
 #define OPERATION_H
 #include <QList>
+#include "structures.h"
 
 class Operation {
 public:
     virtual ~Operation() = default;
     virtual void execute() = 0; // 纯虚函数，子类必须实现
+
+    QString sql;          // 原始 SQL 语句
+    QString statementType;// 语句类型
+    LogRecord logRecord;  // 日志记录
+
 };
 #endif // OPERATION_H
